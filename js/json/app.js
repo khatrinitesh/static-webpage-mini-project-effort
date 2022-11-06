1fedb49702334a132e309853accbaea008af7460
nitesh89 <nitesh.khatri88@gmail.com>
2022-11-06 23:43:41 +0530

    added js features

---
+++ js/json/app.js
@@ -0,0 +1,24 @@
+let student = {
+	name:"nitesh",
+	age:33,
+	city:"Mumbai"
+}
+
+let JData = (JSON.stringify(student))
+console.log(JData)
+
+// let data = `{
+// 	"name":"nitesh",
+// 	"age":33,
+// 	"is_student":true,
+// 	"passport_no":null,
+// 	"p_lang":["c","c++","java","python"],
+// 	"address":{
+// 		"city":"delhi",
+// 		"state":"new_delhi",
+// 		"pincode":110011
+// 	}
+// }`
+
+// let resultdata = JSON.parse(data)
+// console.log(resultdata["address"]["city"])
---
+++ js/json/data2.js
@@ -0,0 +1,12 @@
+{
+	"name":"nitesh",
+	"age":33,
+	"is_student":true,
+	"passport_no":null,
+	"p_lang":["c","c++","java","python"],
+	"address":{
+		"city":"delhi",
+		"state":"new delhi",
+		"pincode":100011,
+	}
+}
